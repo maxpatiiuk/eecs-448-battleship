051fabe3b1cb96beed76cc948af1a00bacc57a0a
Austin-Ng105 <71210473+Austin-Ng105@users.noreply.github.com>
2021-09-18 14:13:26 -0500

    Implemented Game Over View

--- eecs-448-battleship/views/GameOverView/scripts.js
+++ eecs-448-battleship/views/GameOverView/scripts.js
@@ -7,26 +7,30 @@ class GameOverView extends View {
   constructor(options) {
     super(options);
 
-    const {
-      // A boolean, whether the player won or lost
-      win,
-    } = options;
   }
   async render(
     // Container would be populated with elements from index.html
     container
   ) {
     await super.render(container);
-
     /* TODO: Display the appropriate lose/win message */
     /* TODO: Listen for "Play again" button click */
     /* TODO: Once button is clicked, call:
-         new MainView().render(container) */
+    new MainView().render(this.container) */
+
+    this.container.getElementsByTagName('p')[0].textContent = this.options.win ? "You Win" : "You Lose";
+
+    this.button = this.container.getElementsByTagName('button')[0];
+    this.Onclick = ()=>new MainView().render(this.container);
+    this.button.addEventListener("click", this.Onclick);
 
     return this;
   }
   remove() {
     super.remove();
-    /* TODO: Remove click event listeners (event.removeEventListener) */
+    /* TODO: Remove cl
+    ick event listeners (event.removeEventListener) */
+    this.button.removeEventListener("click", this.Onclick);
+
   }
 }
