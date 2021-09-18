88b1fc35a6fe106dc4524cc026f682fcef63ef81
Zachary McCray <zachmccray@ku.edu>
2021-09-18 11:46:46 -0500

    Created Basic GameBoard Structure

--- eecs-448-battleship/views/GameBoardView/scripts.js
+++ eecs-448-battleship/views/GameBoardView/scripts.js
@@ -16,21 +16,44 @@ class GameBoardView extends View {
       board,
     } = options;
   }
+
   async render(
     // Container would be populated with elements from index.html
     container
   ) {
     await super.render(container);
-
+    
+    
     /* TODO: Render the 9x10 grid */
     /*
      * TODO: once game is finished, call:
      *   new GameOverView({win: true}).render(container)
      *   (where win is true, if player won, else false)
      */
-
+    
     return this;
   }
+
+  async load()
+  {
+    document.addEventListener("DOMContentLoaded", () => {
+      board = document.getElementsByClassName("game-board")[0];
+      for(let rows = 1; rows <= 9; rows++)
+        {
+          for(let cols = 1; cols <= 10; cols++)
+          {
+            var boardElement = document.createElement("div");
+            boardElement.setAttribute("row", rows);
+            boardElement.setAttribute("col", cols);
+            boardElement.setAttribute("class", "space");
+            board.appendChild(boardElement);
+          }
+        }
+    });
+  }
+
+
+
   remove() {
     super.remove();
     /* TODO: Remove click event listeners (event.removeEventListener) */
